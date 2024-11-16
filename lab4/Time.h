1f22984133364a32f9e65c89df7e0a3c1ca6ab67
KuvarzinMA <118750083+KuvarzinMA@users.noreply.github.com>
2024-11-16 14:40:29 +0500

    Update Time.h

--- lab4/Time.h
+++ lab4/Time.h
@@ -3,44 +3,45 @@
 
 #include <iostream>
 
-class Time {
+class Time { // Объявление класса Time
 private:
-    short int hours;
-    short int minutes;
+    short int hours;   // Приватное поле для хранения количества часов
+    short int minutes; // Приватное поле для хранения количества минут
 
 public:
     // Конструкторы (Фигурные скобки позволяют задать начальные значения полям класса)
-    Time();
-    Time(short int h, short int m);
-    Time(const Time& other);
+    Time(); // Конструктор по умолчанию, инициализирующий объект Time
+    Time(short int h, short int m); // Конструктор, принимающий часы и минуты в качестве параметров
+    Time(const Time& other); // Конструктор копирования, создающий новый объект на основе существующего
 
     // Сеттер - это метод, который устанавливает значение для приватного поля
-    void setTime(short int h, short int m);
+    void setTime(short int h, short int m); // Метод для установки значений часов и минут
 
-    // Геттеры - это метод который возвращает значение приватного поля
-    short int getHours() const;
-    short int getMinutes() const;
+    // Геттеры - это методы, которые возвращают значение приватного поля
+    short int getHours() const; // Метод для получения значения часов
+    short int getMinutes() const; // Метод для получения значения минут
 
     // Методы
-    Time subtract(const Time& other) const;
-    void inputTime();
+    Time subtract(const Time& other) const; // Метод для вычитания времени другого объекта Time
+    void inputTime(); // Метод для ввода времени от пользователя
 
     // Перегрузка оператора вывода
-    friend std::ostream& operator<<(std::ostream& os, const Time& time);
+    friend std::ostream& operator<<(std::ostream& os, const Time& time); // Дружеская функция для перегрузки оператора вывода
 
     // Перегрузка унарных операторов
-    Time& operator++();    // Префиксный инкремент
-    Time operator++(int);  // Постфиксный инкремент
-    Time& operator--();    // Префиксный декремент
-    Time operator--(int);  // Постфиксный декремент
+    Time& operator++();    // Префиксный инкремент (увеличивает время на 1 минуту)
+    Time operator++(int);  // Постфиксный инкремент (увеличивает время на 1 минуту, возвращая старое значение)
+    Time& operator--();    // Префиксный декремент (уменьшает время на 1 минуту)
+    Time operator--(int);  // Постфиксный декремент (уменьшает время на 1 минуту, возвращая старое значение)
 
     // Перегрузка операций приведения типа
-    operator int() const;    // Преобразование в int (общее количество минут)
-    explicit operator bool() const; // Преобразование в bool (проверка на ноль)
+    operator int() const;    // Преобразование объекта Time в int (общее количество минут)
+    explicit operator bool() const; // Преобразование объекта Time в bool (проверка на ноль, если время равно 0), explicit используется для предотвращения ошибки при преобразовании 
 
     // Перегрузка бинарных операторов сравнения
-    bool operator<(const Time& other) const;
-    bool operator>(const Time& other) const;
+    bool operator<(const Time& other) const; // Сравнение времени: меньше ли текущее время, чем другое
+    bool operator>(const Time& other) const; // Сравнение времени: больше ли текущее время, чем другое
 };
+
 
 #endif // TIME_H
